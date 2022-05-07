0da8d9ec47995988f6a58f8f7995edfd4e79500d
fagneraureliano <fagner2aureliano@gmail.com>
2022-05-07 13:32:06 -0300

    feat(CloseButton):aula 02 impl closebtn e imgs

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,10 +1,58 @@
+import { CloseButton } from "./CloseButton";
+import bugImageUrl from "../assets/bug.svg";
+import ideaImageUrl from "../assets/idea.svg";
+import thoughtImageUrl from "../assets/thought.svg";
+
+const feedbacTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outros",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de uma núvem de pensamento",
+    },
+  },
+};
 export function WidgetForm() {
+
+  
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
         <span className="text-xl leading-6">Deixe seu feedback</span>
+        <CloseButton />
       </header>
-      <p> Hello world</p>
+
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbacTypes).map(([key, value]) => {
+          return (
+            <button
+              key={key}
+              className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none    "
+              // onClick={}
+              type="button"
+            >
+              <img src={value.image.source} alt={value.image.alt} />
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
+
       <footer className="text-xs text text-neutral-400">
         Feito com ♥ pela{" "}
         <a href="" className="underline underline-offset-2">
